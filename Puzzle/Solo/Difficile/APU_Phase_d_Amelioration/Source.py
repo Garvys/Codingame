986bea7f5d9fc5bce7ae6abcc2a5bb576e116a47
Alexandre Caulier <Alexandre.Caulier@ensimag.grenoble-inp.fr>
2016-02-08 16:45:20 +0100

    APU Phase d'Amélioration : can't crossing links

--- Puzzle/Solo/Difficile/APU_Phase_d_Amelioration/Source.py
+++ Puzzle/Solo/Difficile/APU_Phase_d_Amelioration/Source.py
@@ -7,6 +7,7 @@ height = int(input())
 
 dictNumToPointInit = dict()
 dictPointToNumInit = dict()
+dictLinksInit = dict()
 cells = list()
 
 def hashPoint(p):
@@ -22,6 +23,9 @@ for i in range(height):
 			dictNumToPointInit[int(c)].append([i,j])
 			dictPointToNumInit[hashPoint([i,j])] = int(c)
 			cells.append([i,j])
+
+for cell in cells:
+	dictLinksInit[hashPoint(cell)] = list()
 	
 def isOnGrid(p):
 	i = p[0]
@@ -57,10 +61,12 @@ class Grid:
 	# dictP2N : dictionnaire qui a un point associe le nombre de lien nécessaire
 	# dictN2P : dictionnaire qui a un nombre de lien nécessaire associe une liste de point
 	# listStrings : liste de strings qui seront a afficher à la fin du programme
+	# dictLinks : dict qui contient les liens d'un poit a l'autre
 
-	def __init__(self,dictP2N,dictN2P):
+	def __init__(self,dictP2N,dictN2P,dictLinks):
 		self.dictP2N = copy.deepcopy(dictP2N)
 		self.dictN2P = copy.deepcopy(dictN2P)
+		self.dictLinks = copy.deepcopy(dictLinks)
 		self.listStrings = list()
 
 	# Solve la grille
@@ -75,14 +81,15 @@ class Grid:
 		for cell in self.dictN2P[1]:
 			listVoisinPositiv = [p for p in getVoisin(cell) if self.dictP2N[hashPoint(p)] > 0]
 			for i in range(len(listVoisinPositiv)):
-				grid = Grid(self.dictP2N,self.dictN2P)
-				grid.addLink(cell,listVoisinPositiv[i])
-				grid.solve()
-				if grid.isSolved():
-					self.dictP2N = copy.deepcopy(grid.dictP2N)
-					self.dictN2P = copy.deepcopy(grid.dictN2P)
-					self.listStrings.extend(grid.listStrings)
-					return
+				if self.isLinkAddable(cell,listVoisinPositiv[i]):
+					grid = Grid(self.dictP2N,self.dictN2P,self.dictLinks)
+					grid.addLink(cell,listVoisinPositiv[i])
+					grid.solve()
+					if grid.isSolved():
+						self.dictP2N = copy.deepcopy(grid.dictP2N)
+						self.dictN2P = copy.deepcopy(grid.dictN2P)
+						self.listStrings.extend(grid.listStrings)
+						return
 
 	#Affiche sur la sortie standard les liens a ajouter pour solver la grille
 	def printLinksToAdd(self):
@@ -94,9 +101,9 @@ class Grid:
 
 	def decreaseNumberNode(self,p):
 		c = self.dictP2N[hashPoint(p)]
-		print(self.dictN2P,file=sys.stderr)
-		print(p,file=sys.stderr)
-		print(c,file=sys.stderr)
+		#print(self.dictN2P,file=sys.stderr)
+		#print(p,file=sys.stderr)
+		#print(c,file=sys.stderr)
 		self.dictN2P[c].remove(p)
 		self.dictN2P[c-1].append(p)
 		self.dictP2N[hashPoint(p)] -= 1
@@ -104,7 +111,35 @@ class Grid:
 	def addLink(self,p1,p2):
 		self.decreaseNumberNode(p1)
 		self.decreaseNumberNode(p2)
+		self.dictLinks[hashPoint(p1)].append(p2)
+		self.dictLinks[hashPoint(p2)].append(p1)
 		self.listStrings.append("{} {} {} {} 1".format(p1[1],p1[0],p2[1],p2[0]))
+
+	def isLinkAddable(self,p1,p2):
+		print("Test add link between",p1,"and",p2,file=sys.stderr)
+		if p1[0] == p2[0]: #Meme ligne
+			colMin = min(p1[1],p2[1])+1
+			colMax = max(p1[1],p2[1])
+			i = p1[0]
+			for j in range(colMin,colMax):
+				up = list()
+				down = list()
+				getVoisinInDir(i,j,-1,0,up)
+				getVoisinInDir(i,j,1,0,down)
+				if len(up) != 0 and len(down) != 0 and down[0] in self.dictLinks[hashPoint(up[0])]:
+					return False
+		else: #Meme colonne
+			lineMin = min(p1[0],p2[0])+1
+			lineMax = max(p1[0],p2[0])
+			j = p1[1]
+			for i in range(lineMin,lineMax):
+				left = list()
+				right = list()
+				getVoisinInDir(i,j,0,-1,left)
+				getVoisinInDir(i,j,0,1,right)
+				if len(left) != 0 and len(right) != 0 and left[0] in self.dictLinks[hashPoint(right[0])]:
+					return False
+		return True
 
 	def getCellPos(self):
 		return [p for p in cells if self.dictP2N[hashPoint(p)] > 0]
@@ -118,18 +153,19 @@ class Grid:
 			listVoisin = getVoisin(cell)
 			if self.dictP2N[hashPoint(cell)] == sum([min(self.dictP2N[hashPoint(p)],2) for p in listVoisin]):
 				for voisin in listVoisin:
-					for oo in range(min(2,self.dictP2N[hashPoint(voisin)])):
-						self.addLink(cell,voisin)
-						numAdded += 1
+					if self.isLinkAddable(cell,voisin):
+						for oo in range(min(2,self.dictP2N[hashPoint(voisin)])):
+							self.addLink(cell,voisin)
+							numAdded += 1
 		for cell in self.dictN2P[1]:
 			listVoisinPositiv = [ p for p in getVoisin(cell) if self.dictP2N[hashPoint(p)] > 0]
-			if len(listVoisinPositiv) == 1:
+			if len(listVoisinPositiv) == 1 and self.isLinkAddable(cell,listVoisinPositiv[0]):
 				self.addLink(cell,listVoisinPositiv[0])
 				numAdded += 1
 		return numAdded
 
 
-grid = Grid(dictPointToNumInit,dictNumToPointInit)
+grid = Grid(dictPointToNumInit,dictNumToPointInit,dictLinksInit)
 
 grid.solve()
 grid.printLinksToAdd()
